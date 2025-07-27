89047af1db9b93ee8558fe5a5e6afb4d3ce08c7a
knowencodelab <cosmasberiknowen@gmail.com>
2025-07-27 12:44:51 +0300

    Add .next/static/chunks/src_app_admin_page_tsx_564ca4a6._.js 1753609490787

---
+++ .next/static/chunks/src_app_admin_page_tsx_564ca4a6._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1be63ca9._.js",
+  "static/chunks/node_modules_2f3bdd8f._.js"
+],
+    source: "dynamic"
+});
